e111ea526da7bcf59f8f3e04d434a23dfd0edd0f
Stef <sstefin@bk.ru>
2020-07-19 02:27:05 -0500

    :bento: Added main funtion

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/IamStefin/Instagram-Image-Downloader'
 EMAIL = 'sstefin@bk.ru'
 AUTHOR = 'Stef'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
